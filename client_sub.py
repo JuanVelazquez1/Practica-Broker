be624bdfcc7e62394672a42762afd6d89aec8630
DIOSURNO <47272005+DIOSURNO@users.noreply.github.com>
2022-11-20 20:13:03 +0100

    Práctica

---
+++ client_sub.py
@@ -0,0 +1,50 @@
+import socket
+import sys
+import select
+import errno
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+broker_address = ('localhost', 10000)
+port = 10001
+messageA = b'sub:id:%i'%port
+messageB = b'sub:topic:game'
+
+# Variable to control if the port selected is free
+connected = False
+
+try:
+    # Send data
+    print('sending {!r}'.format(messageA))
+    sent = sock.sendto(messageA, broker_address)
+    print('sending {!r}'.format(messageB))
+    sent = sock.sendto(messageB, broker_address)
+finally:
+    print('closing socket')
+    sock.close()
+
+# Bind the socket to the port
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+while not connected:
+    try:
+        server_address = ('localhost', port)
+        sock.bind(server_address)
+        connected = True
+        print('starting up on {} port {}'.format(*server_address))
+        
+    # If bind gives an error, it means that the port is already in use
+    # so we try with the next one, until one is free
+    except socket.error as error:
+        if error.args[0] == errno.EADDRINUSE:
+            port += 1
+
+
+while True:
+    print('\nwaiting to receive message')
+    rlist, _, _ = select.select([sock], [], []) 
+    data, address = sock.recvfrom(1024)
+    # Do stuff with data, fill this up with your code
+    print('received {} bytes from {}'.format(len(data), address))
+    print(data)
+
+
